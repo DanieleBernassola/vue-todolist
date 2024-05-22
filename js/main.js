51216a64cc7317c02ff75a099b23d460214b6b62
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-22 18:37:49 +0200

    [UPD] Completed milestone 3

--- js/main.js
+++ js/main.js
@@ -26,12 +26,21 @@ createApp({
           done: false,
         },
       ],
+      // Stringa in cui verrà salvato il valore dell'input
+      newToDo: "",
     };
   },
   methods: {
     removeItem(index) {
       // Splice rimuove n elementi dall'array (senza l'1 avrebbe rimosso l'elemento più tutti i successivi)
       this.toDoList.splice(index, 1);
+    },
+    addItem() {
+      // Se la stringa non è vuota aggiungo l'input nell'array, lo metto a false e reinizializzo l'input a vuoto
+      if (this.newToDo !== "") {
+        this.toDoList.push({ text: this.newToDo, done: false });
+        this.newToDo = "";
+      }
     },
   },
 }).mount("#app");
